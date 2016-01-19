b7604865597eda15c1f31416541150199607a28a
mageia <yzg963@gmail.com>
2016-01-20 01:41:36 +0800

    ex3

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -9,6 +9,7 @@ num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
+X = [ones(m, 1) X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -21,9 +22,7 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
+[M, p] = max(sigmoid([ones(m,1) sigmoid(X * Theta1')] * Theta2'), [], 2);
 
 
 
